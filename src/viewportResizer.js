7393c58721c4e9cc12d0cb30a0b7bc6a2d7a37ca
kaptinlin <kaptinlin@gmail.com>
2012-09-30 01:00:06 -0700

    change the url functions struction.

--- src/viewportResizer.js
+++ src/viewportResizer.js
@@ -1,74 +1,8 @@
 (function (window, document, $, undefined) {
   "use strict";
 
-  function UriParser(uri) {
-    if (uri === undefined) {
-      uri = window.location.href;
-    }
-
-    function get_hash() {
-      var hash = uri.match(/#([^\s]+)/);
-
-      if (hash !== null) {
-        return hash.pop();
-      } else {
-        return null;
-      }
-    }
-
-    function get_query() {
-      var _uri = uri.replace(/#[^\s]+/, '');
-      var query = {};
-      var query_string = _uri.split('?')[1];
-      if (query_string !== undefined) {
-        query_string = query_string.split('&');
-
-        $.each(query_string, function (i, arg_string) {
-          var argument = arg_string.split('=');
-          query[argument[0]] = argument[1];
-        });
-      }
-      return query;
-    }
-
-    function set_query(value) {
-
-    }
-
-    function set_hash(value) {
-
-    }
-
-    return {
-      get: function (type) {
-        switch (type) {
-        case 'query':
-          return get_query();
-        case 'hash':
-          return get_hash();
-        default:
-          return uri;
-        }
-      },
-      set: function (type, value) {
-        switch (type) {
-        case 'query':
-          return set_query(value);
-        case 'hash':
-          return set_hash(value);
-        }
-      }
-    };
-  }
-
   // Constructor
   var self = $.ViewportResizer = function (options) {
-
-    // if (typeof options === 'string') {
-    //   options = {
-    //     target: options
-    //   };
-    // }
     $.extend(self.settings, options);
     var swithcer, iframe, uri;
 
@@ -80,7 +14,6 @@
     self.query = {};
 
     // Private Methods
-
     function init() {
       swithcer.build();
       iframe.build();
@@ -90,7 +23,7 @@
       });
 
 
-      if (typeof self.settings.target !== 'undefined') {
+      if (self.settings.target !== undefined) {
         uri.init(self.settings.target);
       } else {
         uri.init();
@@ -111,7 +44,7 @@
     // bind history event
     window.onpopstate = function (event) {
       if (event.state !== null) {
-        if (typeof event.state.viewport !== 'undefined') {
+        if (event.state.viewport !== undefined) {
           switchTo(event.state.viewport, false);
         }
       }
@@ -125,7 +58,6 @@
     }
 
     // thank to http://chris-spittles.co.uk/?p=531
-
     function getScrollbarWidth() {
       var $inner = jQuery('<div style="width: 100%; height:200px;">test</div>'),
           $outer = jQuery('<div style="width:200px; height:150px; position: absolute; top: 0; left: 0; visibility: hidden; overflow:hidden;"></div>').append($inner),
@@ -143,19 +75,17 @@
 
     uri = {
       init: function (target) {
-        var $uriParser = UriParser();
+        $.extend(self.query, uri.getQuery());
 
-        $.extend(self.query, $uriParser.get('query'));
-
-        if (typeof self.query.viewport !== 'undefined') {
-          if (typeof self.settings.sizes[self.query.viewport] !== 'undefined') {
+        if (self.query.viewport !== undefined) {
+          if (self.settings.sizes[self.query.viewport] !== undefined) {
             switchTo(self.query.viewport, false);
           }
         }
 
-        if (typeof self.query.target !== 'undefined') {
+        if (self.query.target !== undefined) {
           iframe.load(self.query.target);
-        } else if (typeof target !== 'undefined') {
+        } else if (target !== undefined) {
           iframe.load(target);
         }
       },
@@ -168,6 +98,22 @@
           var query_string = '?' + query_array.join('&');
           window.history.pushState(query, "", query_string);
         }
+      },
+      getQuery: function (url) {
+        if (url === undefined) {
+          url = window.location.href;
+        }
+        var query = {};
+        var query_string = url.replace(/#[^\s]+/, '').split('?')[1];
+        if (query_string !== undefined) {
+          query_string = query_string.split('&');
+
+          $.each(query_string, function (i, arg_string) {
+            var argument = arg_string.split('=');
+            query[argument[0]] = argument[1];
+          });
+        }
+        return query;
       }
     };
 
